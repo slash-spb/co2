9cd013932569d4dc840135b594d722615008ed07
slash_spb <slash_spb@inbox.ru>
2020-12-23 00:54:24 +0300

    WORK, but no detection breaser on or off

--- main_nano.c
+++ main_nano.c
@@ -11,8 +11,12 @@
 #define BAUD 9600
 #define MYUBRR F_CPU/16/BAUD-1
 
-volatile char time;
-volatile char rx_uart_data_i;
+volatile char           time;
+volatile char           rx_uart_data_i;
+
+volatile unsigned char  m_t;
+volatile unsigned short m_co2;
+volatile char           m_en = 0;
 
 static int uart_putchar(char c, FILE *stream);
 static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
@@ -53,6 +57,9 @@ void io_init()
 {
   //Led pin mode out
   DDRB |= (1 << DDB5);
+
+  PORTC &= ~(1 << DDC0);
+  DDRC |= (1 << DDC0);
 }
 
 void timer_init()
@@ -85,10 +92,141 @@ void setup()
 
 void main()
 {
+  char state = 0;
+  unsigned short co2_str;
+  unsigned char co2_anomalie_num = 0;
+  unsigned char t=0;
+
   setup();
   stdout = &mystdout;
   // основная программа
-  while(1);
+  while(1) {
+    if ( m_en ) {
+      printf("\nCO2: %d, T: %d, S: %d, CO2str:%d, I:%d\n",m_co2,m_t,state, co2_str, t);
+      //Reset measure enable flag
+      m_en = 0;
+     
+      //Time 
+      t++;
+
+      switch( state )
+      {
+        case 0:
+          if ( (t%50) == 49  ) {
+            t=0;
+            if ( m_co2 > 800 ) {
+               //Turn on breather
+              PORTC |= (1 << DDC0);
+              _delay_ms(100);
+              PORTC &= ~(1 << DDC0);
+              
+              //Write start value for CO2 too high state
+              co2_str = m_co2;
+              state = 2;
+            } else {
+              co2_str = m_co2;
+
+              state = 1;
+            }
+          }
+          
+          break;
+        case 1://CO2 normal, breather should be switch off
+
+          /*
+          //Test if breather no turn off.
+          //We get m_co2 not increasing so
+          //try to turn off breather other one
+          if ( co2_anomalie_num > 100 ) {
+            //Turn on breather
+            PORTC |= (1 << DDC0);
+            _delay_ms(100);
+            PORTC &= ~(1 << DDC0);
+
+            co2_anomalie_num = 0;
+          }
+          if ( m_co2 < (co2_str+30) )
+            co2_anomalie_num++;
+          else
+            co2_anomalie_num = 0;
+         */
+
+          if ( ( t % 100 ) == 99 ) {
+            if ( (co2_str-m_co2) > -10 ) { //CO2 decease, need turn off breaser
+              //Turn off breather
+              PORTC |= (1 << DDC0);
+              _delay_ms(100);
+              PORTC &= ~(1 << DDC0);
+
+              co2_str = m_co2;
+              t=0;
+            }
+          }
+         
+          if ( m_co2 > 800 ) {
+
+            //Turn on breather
+            PORTC |= (1 << DDC0);
+            _delay_ms(100);
+            PORTC &= ~(1 << DDC0);
+            
+            //Write start value for CO2 too high state
+            co2_str = m_co2;
+            state = 2;
+          }
+          
+          break;
+        case 2://CO2 too high, breather should be switch on
+
+          if ( ( t % 100 ) == 99 ) {
+            if ( (co2_str-m_co2) < -10 ) { //CO2 increase, need turn on breaser
+              //Turn on breather
+              PORTC |= (1 << DDC0);
+              _delay_ms(100);
+              PORTC &= ~(1 << DDC0);
+
+              co2_str = m_co2;
+              t=0;
+            }
+          }
+          
+          /*
+          //Test if breather not turn on.        
+          //We get m_co2 not decreasing so
+          //try to turn on breather other one
+          if ( co2_anomalie_num > 100 ) {
+            //Turn on breather
+            PORTC |= (1 << DDC0);
+            _delay_ms(100);
+            PORTC &= ~(1 << DDC0);
+
+            co2_anomalie_num = 0;
+          }
+         
+          if ( m_co2 > co2_str )
+            co2_anomalie_num++;
+          else
+            co2_anomalie_num = 0;
+          */
+
+          //We get proper CO2 level
+          if ( m_co2 < 650 ) {
+            //Turn off breather
+            PORTC |= (1 << DDC0);
+            _delay_ms(100);
+            PORTC &= ~(1 << DDC0);
+
+            co2_str = 650;
+
+            state = 1;
+          }
+
+          break;
+        default :
+          break;
+      }
+    }
+  }
 }
 
 volatile unsigned char          rx_uart_data [9];
@@ -101,6 +239,10 @@ ISR(TIMER1_COMPA_vect)
   if( (time % 5) == 0 ) {
     PORTB ^= (1 << DDB5);
 
+    PORTC &= ~(1 << DDC0);
+    //_delay_ms(100);
+    //PORTC |= (1 << DDC0);
+
     for(i=0; i<9;i=i+1) {
       USART_Transmit(read_co2_cmd[i]);
       rx_uart_data_i = 0;
@@ -108,8 +250,6 @@ ISR(TIMER1_COMPA_vect)
   }
   time++;
 }
-
-unsigned short co2;
 
 ISR(USART_RX_vect)
 {
@@ -121,7 +261,11 @@ ISR(USART_RX_vect)
   rx_uart_data_i++;
   
   if ( rx_uart_data_i == 9 ) {
-    co2 = (rx_uart_data[2]<<8)+rx_uart_data[3];
-    printf("\nCO2: %d, T: %d\n",co2,rx_uart_data[4]-40);
+    //Co2 value
+    m_co2 = (rx_uart_data[2]<<8)+rx_uart_data[3];
+    //Temperature value in C
+    m_t   = rx_uart_data[4]-40;
+    //Measure enable
+    m_en  = 1;
   }
 }
